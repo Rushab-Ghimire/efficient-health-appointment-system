869143e76ee75cb38b55435d834bc8f02ccbd24a
Rushab Ghimire <rushabghimire@gmail.com>
2025-07-11 13:32:16 +0545

    Switched from db.sqllite with PostgreSql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'health_system_user',   
+        'NAME': 'health_system_db',       
+        'PASSWORD': 'mysecretpassword',  
+        'HOST': 'localhost',            
+        'PORT': '5432',                  
     }
 }
 
